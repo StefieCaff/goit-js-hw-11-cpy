d77350c3ef3fad3f96b8a3277f87be0052b1a8ce
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-19 11:21:03 -0800

    plesae let this work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+//------------------local---------------------------------
 import './css/styles.css';
+import renderGallery from './render-gallery';
+
+//------------------libraries------------------------------
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const axios = require('axios').default;
+
+const userInput = document.querySelector('.searchQuery');
+const API_KEY = '33708941-9afad2bda68efbaf1594840f2';
+const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + userInput + "&image_type=photo&orientation=horizontal&safesearch=true";
+
+
+
+// $.getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
+
+
+
+
+//--------------initialize------------------------------
+
+//simple lightbox image library
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, throttleInterval: 100 });
---
+++ src/render-gallery.js
@@ -0,0 +1,44 @@
+let renderGallery = '';
+export default renderGallery = images => {
+
+  const gallery = document.querySelector('.gallery');
+  const markup = images
+    .map((image) => {
+  
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads
+      } = imgParams;
+      return (gallery.innerHTML =
+        `
+          <div class="photo-card">
+            <a class="gallery__item" href="${largeImageURL}">
+              <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes: ${likes}</b>
+              </p>
+              <p class="info-item">
+                <b>Views: ${views}</b>
+              </p>
+              <p class="info-item">
+                <b>Comments: ${comments}</b>
+              </p>
+              <p class="info-item">
+                <b>Downloads: ${downloads}</b>
+              </p>
+            </div>
+          </div>
+
+        `
+      )
+    })
+    .join("");
+  gallery.innerHTML = markup;
+};
