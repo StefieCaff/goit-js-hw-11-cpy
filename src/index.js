7f0a2c370cc45764c01cb8b2a48b23ba2e90ea6c
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-19 18:55:55 -0800

    get request bones

--- src/index.js
+++ src/index.js
@@ -3,28 +3,48 @@ import './css/styles.css';
 import renderGallery from './render-gallery';
 
 //------------------libraries------------------------------
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-// const axios = require('axios').default;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
 
-const userInput = document.querySelector('.searchQuery');
+const userInput = document.querySelector('input#search-query');
 const API_KEY = '33708941-9afad2bda68efbaf1594840f2';
-const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + userInput + "&image_type=photo&orientation=horizontal&safesearch=true";
+const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + userInput.value + "&image_type=photo&orientation=horizontal&safesearch=true";
+const form = document.querySelector('form');
+
+
+//------------API request-------------------------
+async function getImages() {
+    try {
+      const response = await axios.get(URL);
+      console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+//-----------helper functions---------------------
+
+const handleSubmit = e => {
+    e.preventDefault();
+    let input = userInput.value.trim();
+
+    if (input.value === "") {
+        return;
+    };
+    console.log(input);
+    return response = getImages(input);
+};
 
 
 
-// $.getJSON(URL, function(data){
-// if (parseInt(data.totalHits) > 0)
-//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-// else
-//     console.log('No hits');
-// });
 
 
+//--------------initialize/events----------------------------
 
-
-//--------------initialize------------------------------
+form.addEventListener('submit', handleSubmit);
 
 //simple lightbox image library
-//new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, throttleInterval: 100 });
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, throttleInterval: 100 });
