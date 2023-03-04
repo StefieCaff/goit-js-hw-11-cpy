7b61e04c1734752a75d4ce470d274c6775f9e92e
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-03-04 12:53:57 -0800

    styling done

--- src/render-gallery.js
+++ src/render-gallery.js
@@ -17,27 +17,29 @@ export default renderGallery = images => {
       } = image;
 
       return (gallery.innerHTML =
-        `
-          <div class="gallery-card">
-            <a class="gallery__item" href="${largeImageURL}">
-              <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            </a>
-            <div class="info">
-              <p class="info-item">
-                <b>Likes: ${likes}</b>
-              </p>
-              <p class="info-item">
-                <b>Views: ${views}</b>
-              </p>
-              <p class="info-item">
-                <b>Comments: ${comments}</b>
-              </p>
-              <p class="info-item">
-                <b>Downloads: ${downloads}</b>
-              </p>
+        `<ul class=" list gallery-list">
+           <li class="gallery-item">
+            <div class="gallery-card">
+              <a class="gallery__item" href="${largeImageURL}">
+                <img class="gallery__image" src="${webformatURL}" alt="${tags}" width="300" height="200" loading="lazy" />
+              </a>
+              <div class="info">
+                <p class="info-item">
+                  <b>Likes</b> ${likes}
+                </p>
+                <p class="info-item">
+                  <b>Views</b> ${views}
+                </p>
+                <p class="info-item">
+                  <b>Comments</b> ${comments}
+                </p>
+                <p class="info-item last">
+                  <b>Downloads</b> ${downloads}
+                </p>
+              </div>
             </div>
-          </div>
-
+          </li>
+        </ul>
         `
       );
     })
